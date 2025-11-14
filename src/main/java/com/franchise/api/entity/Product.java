d294357b21629b01415f763912aae3ec1bd868ab
felipeacx <juanfe1190@gmail.com>
2025-11-13 20:30:15 -0500

    Agregar entidades de Franquicia, Sucursal y Producto

---
+++ src/main/java/com/franchise/api/entity/Product.java
@@ -0,0 +1,32 @@
+package com.franchise.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entidad que representa un Producto
+ */
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private Branch branch;
+}
