dd182792d36727eac9dc20eb976b93eaf7bc11ce
felipeacx <juanfe1190@gmail.com>
2025-11-14 09:51:23 -0500

    Agregar clase principal para la aplicación Spring Boot de la API de Franquicias

---
+++ src/main/java/com/franchise/api/FranchiseApiApplication.java
@@ -0,0 +1,16 @@
+package com.franchise.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal de la aplicación Spring Boot para la API de Franquicias
+ */
+@SpringBootApplication
+public class FranchiseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FranchiseApiApplication.class, args);
+    }
+}
+
